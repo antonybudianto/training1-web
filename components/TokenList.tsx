4811e1722a05e3f88347f7df1b3b532c54e52097
Antony <antony.budianto@tokopedia.com>
2022-08-31 23:17:46 +0800

    memoize filteredCurrency

--- components/TokenList.tsx
+++ components/TokenList.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FC, useRef } from "react";
+import { FC, useMemo, useRef } from "react";
 
 import useCurrencies from "@/hooks/useCurrencies";
 import usePriceChanges from "@/hooks/usePriceChanges";
@@ -14,6 +14,12 @@ const TokenList: FC<TokenListProps> = ({ search }) => {
   const currencies = useCurrencies();
   const priceChangeMap = usePriceChanges();
   const animRef = useRef<Record<string, string>>({});
+  const filteredCurrencies = useMemo(() => {
+    return currencies.filter((d: CurrencyData) => {
+      if (!search) return true;
+      return d.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+  }, [search, currencies]);
 
   return (
     <div className="mt-10 mx-auto">
@@ -50,89 +56,84 @@ const TokenList: FC<TokenListProps> = ({ search }) => {
           </tr>
         </thead>
         <tbody className="text-sm lg:text-lg">
-          {currencies
-            .filter((d: CurrencyData) => {
-              if (!search) return true;
-              return d.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
-            })
-            .map((d: CurrencyData, i) => {
-              const latest =
-                priceChangeMap[
-                  `${d.currencySymbol.toLowerCase()}/${pairMainCurrency}`
-                ] || {};
-              const day = parseFloat(latest.day) || 0;
-              const dayMinus = day < 0;
-              const week = parseFloat(latest.week) || 0;
-              const weekMinus = week < 0;
-              const month = parseFloat(latest.month) || 0;
-              const monthMinus = month < 0;
-              const year = parseFloat(latest.year) || 0;
-              const yearMinus = year < 0;
-              animRef.current[d.currencySymbol] = dayMinus
-                ? "animate-orange-change"
-                : "animate-green-change";
-              setTimeout(() => {
-                animRef.current[d.currencySymbol] = "";
-              }, 2000);
-              return (
-                <tr key={i}>
-                  <td className="p-3 border-green-400 border-1">
-                    <div className="flex items-center">
-                      <Image
-                        src={d.logo}
-                        alt={`logo ${d.name}`}
-                        width={30}
-                        height={30}
-                        style={{
-                          backgroundColor: "white",
-                        }}
-                      />
-                      <span className="ml-2">{d.name}</span>
-                    </div>
-                  </td>
-                  <td className="text-gray-400">{d.currencySymbol}</td>
-                  <PriceCell
-                    animation={
-                      dayMinus ? "animate-red-change" : "animate-green-change"
-                    }
-                    value={`Rp${locale.format(latest.latestPrice || 0)}`}
-                  />
+          {filteredCurrencies.map((d: CurrencyData, i) => {
+            const latest =
+              priceChangeMap[
+                `${d.currencySymbol.toLowerCase()}/${pairMainCurrency}`
+              ] || {};
+            const day = parseFloat(latest.day) || 0;
+            const dayMinus = day < 0;
+            const week = parseFloat(latest.week) || 0;
+            const weekMinus = week < 0;
+            const month = parseFloat(latest.month) || 0;
+            const monthMinus = month < 0;
+            const year = parseFloat(latest.year) || 0;
+            const yearMinus = year < 0;
+            animRef.current[d.currencySymbol] = dayMinus
+              ? "animate-orange-change"
+              : "animate-green-change";
+            setTimeout(() => {
+              animRef.current[d.currencySymbol] = "";
+            }, 2000);
+            return (
+              <tr key={i}>
+                <td className="p-3 border-green-400 border-1">
+                  <div className="flex items-center">
+                    <Image
+                      src={d.logo}
+                      alt={`logo ${d.name}`}
+                      width={30}
+                      height={30}
+                      style={{
+                        backgroundColor: "white",
+                      }}
+                    />
+                    <span className="ml-2">{d.name}</span>
+                  </div>
+                </td>
+                <td className="text-gray-400">{d.currencySymbol}</td>
+                <PriceCell
+                  animation={
+                    dayMinus ? "animate-red-change" : "animate-green-change"
+                  }
+                  value={`Rp${locale.format(latest.latestPrice || 0)}`}
+                />
 
-                  <td
-                    className={`text-center ${
-                      dayMinus ? "text-red-500" : "text-green-600"
-                    }`}
-                  >
-                    {dayMinus ? "-" : "+"}
-                    {Math.abs(day)}%
-                  </td>
-                  <td
-                    className={`text-center hidden lg:table-cell ${
-                      weekMinus ? "text-red-500" : "text-green-600"
-                    }`}
-                  >
-                    {weekMinus ? "-" : "+"}
-                    {Math.abs(week)}%
-                  </td>
-                  <td
-                    className={`text-center hidden lg:table-cell ${
-                      monthMinus ? "text-red-500" : "text-green-600"
-                    }`}
-                  >
-                    {monthMinus ? "-" : "+"}
-                    {Math.abs(month)}%
-                  </td>
-                  <td
-                    className={`text-center hidden lg:table-cell ${
-                      yearMinus ? "text-red-500" : "text-green-600"
-                    }`}
-                  >
-                    {yearMinus ? "-" : "+"}
-                    {Math.abs(year)}%
-                  </td>
-                </tr>
-              );
-            })}
+                <td
+                  className={`text-center ${
+                    dayMinus ? "text-red-500" : "text-green-600"
+                  }`}
+                >
+                  {dayMinus ? "-" : "+"}
+                  {Math.abs(day)}%
+                </td>
+                <td
+                  className={`text-center hidden lg:table-cell ${
+                    weekMinus ? "text-red-500" : "text-green-600"
+                  }`}
+                >
+                  {weekMinus ? "-" : "+"}
+                  {Math.abs(week)}%
+                </td>
+                <td
+                  className={`text-center hidden lg:table-cell ${
+                    monthMinus ? "text-red-500" : "text-green-600"
+                  }`}
+                >
+                  {monthMinus ? "-" : "+"}
+                  {Math.abs(month)}%
+                </td>
+                <td
+                  className={`text-center hidden lg:table-cell ${
+                    yearMinus ? "text-red-500" : "text-green-600"
+                  }`}
+                >
+                  {yearMinus ? "-" : "+"}
+                  {Math.abs(year)}%
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
